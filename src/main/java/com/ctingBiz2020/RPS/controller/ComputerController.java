42e74ebdc534794439730d8afd58870e747222a3
Chong Ting <cting@iogenesis.com>
2021-07-06 09:07:52 -0500

    complete aws REST API

--- src/main/java/com/ctingBiz2020/RPS/controller/ComputerController.java
+++ src/main/java/com/ctingBiz2020/RPS/controller/ComputerController.java
@@ -1,6 +1,7 @@
 package com.ctingBiz2020.RPS.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import com.ctingBiz2020.RPS.dao.Computer;
 import com.ctingBiz2020.RPS.service.ComputerService;
 
 @Controller
+//@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://project-rps.s3.us-east-2.amazonaws.com")
 @RequestMapping("/")
 public class ComputerController {
 
@@ -20,6 +23,7 @@ public class ComputerController {
 
 		System.out.println(data.toString());
 		Computer newData = computerService.validateResult(data);
+		System.out.println(newData.toString());
 		return newData;
 	}
 
