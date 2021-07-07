8f7bea13452805726b1c889b4a0cb29031329498
Chong Ting <cting@iogenesis.com>
2021-07-07 10:52:15 -0500

    fix default landing and CORS issues

--- src/main/java/com/ctingBiz2020/RPS/controller/ComputerController.java
+++ src/main/java/com/ctingBiz2020/RPS/controller/ComputerController.java
@@ -1,23 +1,22 @@
 package com.ctingBiz2020.RPS.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.ctingBiz2020.RPS.dao.Computer;
 import com.ctingBiz2020.RPS.service.ComputerService;
 
-@Controller
+@RestController
 //@CrossOrigin("http://localhost:3000")
-@CrossOrigin("https://project-rps.s3.us-east-2.amazonaws.com")
-@RequestMapping("/")
+//@CrossOrigin("https://project-rps.s3.us-east-2.amazonaws.com")
+@CrossOrigin
 public class ComputerController {
 
-	@PostMapping("computer")
-	@ResponseBody
+	@PostMapping("/computer")
 	public Computer computerSelection(@RequestBody Computer data) {
 		ComputerService computerService = new ComputerService();
 
@@ -26,7 +25,8 @@ public class ComputerController {
 		System.out.println(newData.toString());
 		return newData;
 	}
-	
+
+	@RequestMapping("/")
 	public String testConncetion() {
 		return "This is Chong backend!";
 	}
